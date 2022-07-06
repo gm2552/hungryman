f9658b029b4bd8243f04dd492ba325a4a4488995
gm2552 <meyerg@vmware.com>
2022-07-06 13:36:55 -0500

    Allowing cross origin requests on availability API resource.

--- hungryman-availability/src/main/java/com/java/example/tanzu/hungryman/resources/AvailabilityResource.java
+++ hungryman-availability/src/main/java/com/java/example/tanzu/hungryman/resources/AvailabilityResource.java
@@ -2,6 +2,7 @@ package com.java.example.tanzu.hungryman.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin
 @RestController
 @RequestMapping("availability")
 @Slf4j

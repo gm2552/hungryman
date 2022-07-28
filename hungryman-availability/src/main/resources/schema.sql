4db8309d9f63ff6a3287048380ac5c288c1c7955
gm2552 <meyerg@vmware.com>
2022-07-28 13:13:52 -0500

    Refactoring DB definitions into a single schema/portable file.

--- hungryman-availability/src/main/resources/schema.sql
+++ hungryman-availability/src/main/resources/schema.sql
@@ -2,6 +2,5 @@ CREATE TABLE IF NOT EXISTS availability ( id bigint NOT NULL AUTO_INCREMENT PRIM
     address VARCHAR(255), locality VARCHAR(255), region VARCHAR(255), postalCode VARCHAR(255), phoneNumber VARCHAR(255), reservationURL VARCHAR(1024),
     requestSubject VARCHAR(255) NOT NULL);
     
-CREATE TABLE IF NOT EXISTS availability_window ( id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY, availabilityId bigint NOT NULL, startTime bigint NOT NULL, 
+CREATE TABLE IF NOT EXISTS availability_window (id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY, availabilityId bigint NOT NULL, startTime bigint NOT NULL, 
     endTime bigint NOT NULL);
-    
